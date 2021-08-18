5f599960a332579959a0bef18507e2025b8d70f9
danyalshah3 <72283767+danyalshah3@users.noreply.github.com>
2021-08-18 10:50:31 -0400

    commit tree 35b246c8bde1f72d3e436c3a083f042248e1b129

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1 = 31
+let num2 = 2
+let num3 = 22
+let num4 = 6
+let set = (1, 10, 13, 20)
 
-// Write your code here
+
+var multiply = num1 * num2
+
+let random = Math.floor(Math.random() * 10) + 1
+
+let mod = num3 % num4
+
+let max = Math.max(set)
